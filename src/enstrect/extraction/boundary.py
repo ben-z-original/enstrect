a58cdeb1643eca0c3aee53519fd9ad9f2f1b1130
Christian Benz <christian.benz@uni-weimar.de>
2024-08-28 10:42:07 +0200

    Refactor boundary.py

--- src/enstrect/extraction/boundary.py
+++ src/enstrect/extraction/boundary.py
@@ -1,48 +1,48 @@
 import alphashape
 import numpy as np
+import open3d as o3d
 from tqdm import tqdm
 import networkx as nx
+from sklearn.cluster import DBSCAN
 from sklearn.decomposition import PCA
-from enstrect.extraction.clustering import get_pcd_classes
 
 
-def extract_bounding_polygons(pcd_pynt, category="corrosion", min_count=2000):
-    defect = np.array(pcd_pynt.points["argmax"])
-    cluster = np.array(pcd_pynt.points["cluster"])
-    
-    pcd_o3d = pcd_pynt.to_instance("open3d", mesh=False, normals=True)
+def extract_bounding_polygons(pcd_pynt, category="corrosion", eps_m=0.01, min_count=2000):
+    points_pd = pcd_pynt.points.loc[pcd_pynt.points[category] == 1].reset_index(drop=False)
+    points_np = np.array(points_pd[['x', 'y', 'z']], dtype=np.float64)
 
-    # get classes of clusters
-    pcd_classes = get_pcd_classes(pcd_pynt)
+    cluster = DBSCAN(eps=eps_m, min_samples=20).fit_predict(points_np)
     cluster_ids, cluster_counts = np.unique(cluster, return_counts=True)
-    cluster_classes = {clu_id: pcd_classes[defect[cluster == clu_id][0]] for clu_id in cluster_ids}
 
     pca = PCA(n_components=2)
     G = nx.Graph()
 
-    relevant_clusters = [key for (key, value) in cluster_classes.items() if value == category]
+    for cluster_id in tqdm(cluster_ids):
+        idxs = np.nonzero(cluster == cluster_id)[0]
 
-    for cluster_id in tqdm(relevant_clusters):
-        if min_count < cluster_counts[cluster_id]:
-            idxs = np.nonzero(cluster == cluster_id)[0]
-            subcloud = pcd_o3d.select_by_index(idxs)
+        points_subcloud = points_np[idxs]
 
-            # extract boundary
-            points = np.array(subcloud.points)
-            points_mapped = pca.fit_transform(points)
-            alpha_shape = alphashape.alphashape(points_mapped, 100)
-            bound_coords = np.array(alpha_shape.boundary.coords)
-            bound_idxs = [np.where((points_mapped == bound_coords[i, ...]).all(axis=1))[0][0]
-                          for i in range(len(bound_coords))]
-            bound_points = points[bound_idxs[:-1]]
+        if False:
+            print()
+            pcd_o3d = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(points_subcloud))
+            o3d.visualization.draw_geometries([pcd_o3d])
+            #subcloud = pcd_o3d.select_by_index(idxs)
 
-            # prepare graph
-            H = nx.Graph()
-            edges = [(i - 1, i) for i in range(1, len(bound_points))] + [(len(bound_points) - 1, 0)]
-            H.add_edges_from(edges)
-            nx.set_node_attributes(H, {i: bound_points[i] for i in range(len(bound_points))}, "pos")
+        # extract boundary
+        points_mapped = pca.fit_transform(points_subcloud)
+        alpha_shape = alphashape.alphashape(points_mapped, 100)
+        bound_coords = np.array(alpha_shape.boundary.coords)
+        bound_idxs = [np.where((points_mapped == bound_coords[i, ...]).all(axis=1))[0][0]
+                      for i in range(len(bound_coords))]
+        bound_points = points_subcloud[bound_idxs[:-1]]
 
-            H = nx.relabel_nodes(H, {key: i + len(G) for i, key in enumerate(H.nodes)}, copy=True)
-            G = nx.compose(G, H)
+        # prepare graph
+        H = nx.Graph()
+        edges = [(i - 1, i) for i in range(1, len(bound_points))] + [(len(bound_points) - 1, 0)]
+        H.add_edges_from(edges)
+        nx.set_node_attributes(H, {i: bound_points[i] for i in range(len(bound_points))}, "pos")
+
+        H = nx.relabel_nodes(H, {key: i + len(G) for i, key in enumerate(H.nodes)}, copy=True)
+        G = nx.compose(G, H)
 
     return G
