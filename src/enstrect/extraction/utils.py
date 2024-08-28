3bb7c0252e4e034cd81f1f2d3a5a6c476521baae
Christian Benz <christian.benz@uni-weimar.de>
2024-08-28 10:41:29 +0200

    Extend utils.py for exposed_rebar

--- src/enstrect/extraction/utils.py
+++ src/enstrect/extraction/utils.py
@@ -8,8 +8,8 @@ def prepare_exposed_rebar(pcd_pynt, eps_m=0.01):
     idxs = np.where(pcd_pynt.points["exposed_rebar"] == 1)[0]
 
     # cluster exposed rebar
-    points = pcd_pynt.points.loc[idxs].reset_index(drop=False)
-    cluster = DBSCAN(eps=eps_m, min_samples=20).fit_predict(np.array(points[['x', 'y', 'z']]))
+    points_pd = pcd_pynt.points.loc[idxs].reset_index(drop=False)
+    cluster = DBSCAN(eps=eps_m, min_samples=20).fit_predict(np.array(points_pd[['x', 'y', 'z']]))
     np.unique(cluster)
 
     for i in np.unique(cluster):
@@ -19,10 +19,17 @@ def prepare_exposed_rebar(pcd_pynt, eps_m=0.01):
         corrosion_count = np.sum(pcd_pynt.points["corrosion"][idxs_cluster])
         exposed_rebar_count = np.sum(pcd_pynt.points["exposed_rebar"][idxs_cluster])
 
-        # readjust classes
-        pcd_pynt.points.loc[idxs_cluster, "spalling"] = np.ubyte(spalling_count == len(idxs_cluster))
-        pcd_pynt.points.loc[idxs_cluster, "corrosion"] = np.ubyte(corrosion_count == len(idxs_cluster))
-        pcd_pynt.points.loc[idxs_cluster, "exposed_rebar"] = np.ubyte(exposed_rebar_count == len(idxs_cluster))
+        # pure spalling
+        if spalling_count == len(idxs_cluster):
+            pcd_pynt.points.loc[idxs_cluster, "exposed_rebar"] = 0
+        # pure corrosion
+        elif corrosion_count == len(idxs_cluster):
+            pcd_pynt.points.loc[idxs_cluster, "exposed_rebar"] = 0
+        # exposed rebar (=combination of spalling and corrosion)
+        else:
+            pcd_pynt.points.loc[idxs_cluster, "exposed_rebar"] = 1
+            pcd_pynt.points.loc[idxs_cluster, "spalling"] = 0
+            #pcd_pynt.points.loc[idxs_cluster, "corrosion"] = 0  # keep corrosion as is
 
     return pcd_pynt
 
