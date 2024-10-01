e2316e739dc6e097060a44b0577505f805852f6a
Christian Benz <christian.benz@uni-weimar.de>
2024-10-01 23:16:28 +0200

    Refactor extraction, fuser, and run method

--- src/enstrect/extraction/utils.py
+++ src/enstrect/extraction/utils.py
@@ -1,6 +1,11 @@
+import torch
 import numpy as np
+import pandas as pd
+import open3d as o3d
+import networkx as nx
 from pyntcloud import PyntCloud
 from sklearn.cluster import DBSCAN
+from pytorch3d.structures import Pointclouds
 
 
 def prepare_exposed_rebar(pcd_pynt, eps_m=0.01):
@@ -35,12 +40,71 @@ def prepare_exposed_rebar(pcd_pynt, eps_m=0.01):
         else:
             pcd_pynt.points.loc[idxs_cluster, "exposed_rebar"] = 1
             pcd_pynt.points.loc[idxs_cluster, "spalling"] = 0
-            #pcd_pynt.points.loc[idxs_cluster, "corrosion"] = 0  # keep corrosion as is
+            # pcd_pynt.points.loc[idxs_cluster, "corrosion"] = 0  # keep corrosion as is
 
     return pcd_pynt
 
 
-def get_pcd_classes(pcd_pynt):
-    removables = ["x", "y", "z", "nx", "ny", "nz", "red", "green", "blue", "argmax", "cluster"]
-    classes = [elem for elem in list(pcd_pynt.points.columns) if elem not in removables]
-    return classes
+def pyt3d_to_pynt(pcd_pyt3d):
+    points_pd = pd.DataFrame(torch.cat([pcd_pyt3d.points_packed().squeeze(),
+                                        pcd_pyt3d.normals_packed().squeeze(),
+                                        pcd_pyt3d.features_packed().squeeze()], axis=1),
+                             columns=["x", "y", "z", "nx", "ny", "nz", "red", "green", "blue"])
+    pcd_pynt = PyntCloud(points_pd)
+    return pcd_pynt
+
+def pynt_to_pyt3d(pcd_pynt, device="cuda:0"):
+    pcd_pyt3d = Pointclouds(torch.tensor(pcd_pynt.points[["x", "y", "z"]].to_numpy()[None, ...],
+                                         dtype=torch.float32, device=device),
+                            torch.tensor(pcd_pynt.points[["nx", "ny", "nz"]].to_numpy()[None, ...],
+                                         dtype=torch.float32, device=device),
+                            torch.tensor(pcd_pynt.points[["red", "green", "blue"]].to_numpy()[None, ...],
+                                         dtype=torch.float32, device=device))
+    return pcd_pyt3d
+
+def G_to_obj(G, polyline_path):
+    if len(G.nodes) == 0: return None
+
+    line_sets = G_to_lineset_o3d(G)
+
+    vertices_obj, lines_obj = "", ""
+    count = 1  # obj starts counting at 1
+
+    for line_set in line_sets:
+        vertices_obj += "".join([f"v {x} {y} {z}\n" for x, y, z in np.array(line_set.points)])
+        lines = np.array(line_set.lines).flatten()
+        lines = np.append(lines[[0]], lines[1:][np.diff(lines) != 0]) + count
+        lines_obj += "l " + " ".join(map(str, lines)) + "\n"
+        count += len(line_set.points)
+
+    with open(str(polyline_path), "w") as f:
+        f.write(vertices_obj + lines_obj)
+
+
+def G_to_lineset_o3d(G, color=[1, 0, 0]):
+    # connected subgraphs
+    S = [nx.convert_node_labels_to_integers(G.subgraph(c), first_label=0) for c in nx.connected_components(G)]
+
+    line_sets = []
+    for SG in S:
+        positions = np.array([SG.nodes[node_id]['pos'] for node_id in SG.nodes])
+        degrees = np.array([val for (node, val) in SG.degree()])
+
+        # is polygon (boundary)
+        if np.all(1 < degrees):
+            cycle_edges = nx.find_cycle(SG)
+            lines = np.array(cycle_edges)
+        # is polyline (centerline)
+        else:
+            src, trg = np.where(degrees == 1)[0]
+            path = nx.shortest_path(SG, source=list(SG.nodes)[src], target=list(SG.nodes)[trg])
+            path = np.int64(np.array(path)) + 1
+            lines = [[path[i] - 1, path[i + 1] - 1] for i in range(len(path) - 1)]
+
+        line_set = o3d.geometry.LineSet()
+        line_set.points = o3d.utility.Vector3dVector(positions)
+        line_set.lines = o3d.utility.Vector2iVector(lines)
+        line_set.paint_uniform_color(color)
+        line_sets.append(line_set)
+
+    return line_sets
--- src/enstrect/run.py
+++ src/enstrect/run.py
@@ -1,28 +1,23 @@
 import warnings
-import open3d as o3d
 from pathlib import Path
 from pytorch3d.io import IO
 from pyntcloud import PyntCloud
 from argparse import ArgumentParser
-from enstrect.mapping.fuser import Fuser, AngleBestFuser, NaiveMaxFuser, NaiveMeanFuser, AngleRangeFuser
+from enstrect.mapping.fuser import Fuser
 from enstrect.mapping.mapper import Mapper
-from utils3d.conversion.lines import G_to_obj
-from utils3d.conversion.cloud import pyt3d_to_pynt
+from enstrect.extraction.utils import G_to_obj
+from enstrect.extraction.utils import pyt3d_to_pynt
 from enstrect.datasets.multiview import MultiviewDataset
 from enstrect.extraction.utils import prepare_exposed_rebar
 from enstrect.segmentation.nnunet_s2ds import NNUNetS2DSModel
-from enstrect.segmentation.omnicrack30k_model import OmniCrack30kModel
 from enstrect.extraction.contraction import extract_centerlines
 from enstrect.extraction.boundary import extract_bounding_polygons
-from utils3d.visualization.orbiting_gif import mesh_o3d_to_gif
 from enstrect.datasets.utils import sample_points_from_meshes_pyt3d
-from utils3d.visualization.visualize_o3d import plot_o3d_mesh_with_lines
 
 warnings.filterwarnings("ignore", message="Detected old nnU-Net plans")  # warning can be ignored
 
 
-def run(obj_or_ply_path, cameras_path, images_dir, out_dir, select_views, num_points, scale,
-        fuser=AngleRangeFuser, model=OmniCrack30kModel):
+def run(obj_or_ply_path, cameras_path, images_dir, out_dir, select_views, num_points, scale):
     # create outdir
     out_dir.mkdir(exist_ok=True)
 
@@ -35,20 +30,16 @@ def run(obj_or_ply_path, cameras_path, images_dir, out_dir, select_views, num_po
         pcd_pynt = PyntCloud.from_file(str(obj_or_ply_path))
 
     # set up segmentation model, mapper, and dataset
-    #model = model()
     model = NNUNetS2DSModel()
-    fuser = fuser(class_weight=model.class_weight)
+    fuser = Fuser(class_weight=model.class_weight)
     mapper = Mapper(model, fuser=fuser)
     dataset = MultiviewDataset(cameras_path, images_dir, select_views=select_views, scale=scale)
 
-    if True:
-        # perform mapping 2D->3D
-        pcd_pynt = mapper(pcd_pynt, dataset, out_dir)
-        pcd_pynt = prepare_exposed_rebar(pcd_pynt)
-        out_dir.mkdir(exist_ok=True)
-        pcd_pynt.to_file(str(Path(out_dir) / f"pcd_{num_points}_processed.ply"))
-    else:
-        pcd_pynt = PyntCloud.from_file(str(Path(out_dir) / f"pcd_{num_points}_processed.ply"))
+    # perform mapping 2D->3D
+    pcd_pynt = mapper(pcd_pynt, dataset)
+    pcd_pynt = prepare_exposed_rebar(pcd_pynt)
+    out_dir.mkdir(exist_ok=True)
+    pcd_pynt.to_file(str(Path(out_dir) / f"pcd_{num_points}_processed.ply"))
 
     # perform extraction
     G_dict = {
@@ -62,40 +53,30 @@ def run(obj_or_ply_path, cameras_path, images_dir, out_dir, select_views, num_po
     for key, G in G_dict.items():
         G_to_obj(G, str(Path(out_dir) / f"{key}.obj"))
 
-    # visualize centerlines and bounding polygons on top of mesh
-    mesh_o3d = o3d.io.read_triangle_mesh(str(obj_or_ply_path), True)
-
-    mesh_o3d_to_gif(mesh_o3d,
-                    G_dict=G_dict,
-                    gif_path=str(Path(out_dir) / f"results.gif"),
-                    off_screen=True,
-                    full_orbit=True if "crackensembles" in str(obj_or_ply_path) else False)
-    # plot_o3d_mesh_with_lines(mesh_o3d, G_dict)
-
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description="""Run enstrect.""")
+    parser = ArgumentParser(description="""Run enstrect for segmenting, mapping, and extraction structural damage.""")
     parser.add_argument('-p', '--obj_or_ply_path', type=Path,
-                        default=Path(__file__).parent / "assets" / "segments" / "bridge_g" /
-                                "segment_dev" / "mesh" / "mesh.obj",
+                        default=Path(__file__).parent / "assets" / "segments" / "bridge_b" /
+                                "segment_test" / "mesh" / "mesh.obj",
                         help="Path to the mesh (.obj; points will be sampled) or point cloud file (.ply).")
     parser.add_argument('-i', '--images_dir', type=Path,
-                        default=Path(__file__).parent / "assets" / "segments" / "bridge_g" /
-                                "segment_dev" / "views",
+                        default=Path(__file__).parent / "assets" / "segments" / "bridge_b" /
+                                "segment_test" / "views",
                         help="Path to the directory which contains the images/views.")
     parser.add_argument('-c', '--cameras_path', type=Path,
-                        default=Path(__file__).parent / "assets" / "segments" / "bridge_g" /
-                                "segment_dev" / "cameras.json",
+                        default=Path(__file__).parent / "assets" / "segments" / "bridge_b" /
+                                "segment_test" / "cameras.json",
                         help="Path to the file that contains the intrinsic and extrinsic camera information.")
     parser.add_argument('-o', '--out_dir', type=Path,
-                        default=Path(__file__).parent / "assets" / "segments" / "bridge_g" /
-                                "segment_dev" / "out",
+                        default=Path(__file__).parent / "assets" / "segments" / "bridge_b" /
+                                "segment_test" / "out",
                         help="Path to the directory where the results will be stored")
-    parser.add_argument('-v', '--select_views', nargs='*', #default=["0005", "0006"],
-                        help="")
-    parser.add_argument('-s', '--scale', type=float, default=1.0, #1.0,  # 1.0,  # 0.25,
-                        help="Rescale the images to be processed. Defaults is 0.25 to reduce runtime.")
-    parser.add_argument('-n', '--num_points', type=int, default=1000000,
+    parser.add_argument('-v', '--select_views', nargs='*', help="List of views that should be used.")
+    parser.add_argument('-s', '--scale', type=float, default=0.25,
+                        help="Rescale the images to be processed. Defaults is 0.25 to reduce runtime. " +
+                             "Use 1.0 for full resolution.")
+    parser.add_argument('-n', '--num_points', type=int, default=10 ** 6,
                         help="Number of points to sample from the mesh. Default is 1,000,000")
     args = parser.parse_args()
 
