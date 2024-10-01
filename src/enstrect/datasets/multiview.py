247ec4112586aa1fcf89566ff143589d631511b9
Christian Benz <christian.benz@uni-weimar.de>
2024-10-01 23:15:03 +0200

    Update datasets

--- src/enstrect/datasets/multiview.py
+++ src/enstrect/datasets/multiview.py
@@ -1,1 +1,44 @@
-../../../../utils3d/src/utils3d/datasets/multiview.py
+import torch
+from pathlib import Path
+from tensordict import TensorDict
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+from torchvision.transforms import v2
+from enstrect.datasets.utils import json_to_pyt3d
+
+
+class MultiviewDataset(Dataset):
+    def __init__(self, cameras_pyt3d_or_path, views_dir=None, select_views=None, scale=1.0, device="cuda:0"):
+        self.scale = scale
+        self.device = device
+        self.views_dir = Path(views_dir) if views_dir is not None else None
+
+        if isinstance(cameras_pyt3d_or_path, (Path, str)):
+            self.view_keys, self.cameras = json_to_pyt3d(cameras_pyt3d_or_path,
+                                                         select_views=select_views,
+                                                         scale=scale, device=device)
+        else:
+            self.cameras = cameras_pyt3d_or_path
+            self.view_keys = [f"{key:04d}" for key in range(len(self.cameras))]
+
+    def __len__(self):
+        return len(self.cameras)
+
+    def __getitem__(self, idx, rgb=False):
+        camera = self.cameras[idx]
+
+        if self.views_dir is not None:
+            img = read_image(str(next(self.views_dir.glob(f"{self.view_keys[idx]}*")))).to(torch.float32)
+            img = img[[2, 1, 0], :, :] if not rgb else img
+        else:
+            img = None
+
+        transforms = v2.Compose([
+            v2.Resize(camera.image_size[0].to(torch.int).tolist()),
+        ])
+        img = transforms(img)
+
+        out = {"name": self.view_keys[idx],
+               "camera": camera,
+               "image": img}
+        return TensorDict(out)
--- src/enstrect/datasets/utils.py
+++ src/enstrect/datasets/utils.py
@@ -1,65 +1,30 @@
+import json
 import torch
-import gdown
-import zipfile
-from pathlib import Path
-from argparse import ArgumentParser
-from pytorch3d.io import load_objs_as_meshes
-from enstrect.datasets.multiview import MultiviewDataset
-from utils3d.conversion.mesh import sample_points_and_save
+from addict import Dict
 from pytorch3d.structures import Pointclouds
+from pytorch3d.renderer import PerspectiveCameras
 from pytorch3d.ops import sample_points_from_meshes
-from pytorch3d.renderer import RasterizationSettings, MeshRasterizer
-from utils3d.analyses.resolution import compute_resolution, compute_required_points
 
 
-def download():
-    parser = ArgumentParser(description="""Downloads the dataset to a given directoy path.""")
-    parser.add_argument('-p', '--segments_path', type=Path,
-                        default=Path(__file__).parents[1] / "assets",
-                        help="Directory where the segments will be downloaded and unzipped.")
-    args = parser.parse_args()
+def json_to_pyt3d(cameras_path, select_views=None, scale=1.0, device="cuda:0"):
+    with open(cameras_path, 'r') as stream:
+        cameras_json = Dict(json.load(stream))
 
-    # doẃnload and unzip plan
-    zippath = Path(args.segments_path / "segments.zip")
-    if not zippath.exists():
-        url = "https://drive.google.com/uc?id=1QkyoZ1o9uKuxpLIlSZ-iA9jcba46oIwW"
-        gdown.download(url, str(zippath), quiet=False)
-        with zipfile.ZipFile(str(zippath), 'r') as zip_ref:
-            zip_ref.extractall(str(args.segments_path / "segments"))
-    else:
-        raise RuntimeError(f"The segments dataset already exists in the path: {zippath}")
-
-
-def sample_points_based_on_resolution(mesh_path, cameras_path, out_ply, device="cuda:0"):
-    meshes_pyt3d = load_objs_as_meshes([mesh_path]).to(device)
-    dataset = MultiviewDataset(cameras_path)
-    camera, min_depth = compute_minimal_distance(meshes_pyt3d, dataset)
-
-    # compute number of required points
-    resolution_mmpx, resolution_mm2_px = compute_resolution(camera, min_depth)
-    num_points = compute_required_points(meshes_pyt3d, resolution_mm2_px)
-
-    sample_points_and_save(meshes_pyt3d, out_ply, num_points=num_points)
-
-
-def compute_minimal_distance(meshes_pyt3d, dataset):
-    rasterizer = MeshRasterizer(raster_settings=RasterizationSettings(blur_radius=0.0, faces_per_pixel=1))
-
-    # loop over views and compute depth/distance
-    cameras, min_depths = [], []
-    for sample in dataset:
-        cam = sample["camera"]
-        rasterizer.raster_settings.image_size = cam.image_size.cpu().to(torch.int64).tolist()[0]
-        fragments = rasterizer(meshes_pyt3d, cameras=cam)
-        depth = fragments.zbuf[0, ..., 0]
-
-        cameras.append(cam)
-        min_depths.append(depth[depth != -1].min())
-
-    min_depths = torch.tensor(min_depths)
-    idx_min = min_depths.argmin().item()
-
-    return cameras[idx_min], min_depths[idx_min]
+    view_keys, cameras = [], []
+    for key, cam in cameras_json.items():
+        if select_views is not None and key not in select_views:
+            continue
+        camera = PerspectiveCameras(
+            focal_length=((scale * cam.focal_length[0][0],
+                           scale * cam.focal_length[0][1]),),
+            principal_point=((scale * cam.principal_point[0][0],
+                              scale * cam.principal_point[0][1]),),
+            image_size=((int(scale * cam.image_size[0][0]),
+                         int(scale * cam.image_size[0][1])),),
+            R=cam.R, T=cam.T, in_ndc=False, device=device)
+        view_keys.append(key)
+        cameras.append(camera)
+    return view_keys, cameras
 
 
 def sample_points_from_meshes_pyt3d(meshes, num_points=1000000):
@@ -69,22 +34,3 @@ def sample_points_from_meshes_pyt3d(meshes, num_points=1000000):
                                                         num_samples=num_points)
     pcd_pyt3d = Pointclouds(points, normals, colors)
     return pcd_pyt3d
-
-
-def sample_points():
-    parser = ArgumentParser(description="""Sample points from mesh according to the best image resolution.""")
-    parser.add_argument('-m', '--mesh_path', type=Path,
-                        default=Path(__file__).parents[1] / "assets" / "segments" / "bridge_b" /
-                                "segment_test" / "mesh" / "mesh.obj",
-                        help="Path to the file that contains the intrinsic and extrinsic camera information.")
-    parser.add_argument('-c', '--cameras_path', type=Path,
-                        default=Path(__file__).parents[1] / "assets" / "segments" / "bridge_b" /
-                                "segment_test" / "cameras.json",
-                        help="Path to the file that contains the intrinsic and extrinsic camera information.")
-    parser.add_argument('-o', '--out_ply', type=Path,
-                        default=Path(__file__).parents[1] / "assets" / "segments" / "bridge_b" /
-                                "segment_test" / "pcd.ply",
-                        help="Path to the file in ply format where the resulting point cloud will be stored.")
-    args = parser.parse_args()
-
-    sample_points_based_on_resolution(args.mesh_path, args.cameras_path, args.out_ply)
