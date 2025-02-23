b8b87f3e1e39acc58702c8953c17cd15c15ca7c9
Christian Benz <christian.benz@uni-weimar.de>
2025-02-23 10:29:21 +0100

    Update download.py

--- src/enstrect/datasets/download.py
+++ src/enstrect/datasets/download.py
@@ -4,23 +4,25 @@ from pathlib import Path
 from argparse import ArgumentParser
 
 
-def download():
-    parser = ArgumentParser(description="""Downloads the dataset to a given directoy path.""")
-    parser.add_argument('-p', '--segments_path', type=Path,
-                        default=Path(__file__).parents[1] / "assets",
-                        help="Directory where the segments will be downloaded and unzipped.")
-    args = parser.parse_args()
-
+def download(name, url, target_path):
     # doẃnload and unzip plan
-    zippath = Path(args.segments_path / "segments.zip")
+    target_path.mkdir(exist_ok=True)
+    zippath = target_path / f"{name}.zip"
     if not zippath.exists():
-        url = "https://drive.google.com/uc?id=1QkyoZ1o9uKuxpLIlSZ-iA9jcba46oIwW"
         gdown.download(url, str(zippath), quiet=False)
         with zipfile.ZipFile(str(zippath), 'r') as zip_ref:
-            zip_ref.extractall(str(args.segments_path / "segments"))
+            zip_ref.extractall(str(target_path / name))
     else:
-        raise RuntimeError(f"The segments dataset already exists in the path: {zippath}")
+        raise RuntimeError(f"The dataset already exists in the path: {zippath}")
 
 
 if __name__ == "__main__":
-    download()
+    parser = ArgumentParser(description="""Downloads the dataset to a given directoy path.""")
+    parser.add_argument('-p', '--target_path', type=Path,
+                        default=Path(__file__).parents[1] / "assets",
+                        help="Directory where the dataset will be downloaded and unzipped.")
+    args = parser.parse_args()
+    
+    download(name="segments",
+             url="https://drive.google.com/uc?id=1QkyoZ1o9uKuxpLIlSZ-iA9jcba46oIwW",
+             target_path=args.target_path)
